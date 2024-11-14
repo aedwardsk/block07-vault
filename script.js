d3d52e7f38ed4374c6ec0498985ae577ee701068
Andy Edwards <andyedwards@Mac.lan>
2024-11-14 14:08:34 -0500

    Added js script

---
+++ script.js
@@ -0,0 +1,15 @@
+// Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+const combination =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+
+// addtion to get num1
+let num1 = 6 + 4;
+// Multiplication to get num2
+let num2 = 5 * 8;
+// Division to get num3
+let num3 = 195 / 5;
+
+// Creating alert box
+alert(combination + num1 + " " + num2 + " " + num3);
